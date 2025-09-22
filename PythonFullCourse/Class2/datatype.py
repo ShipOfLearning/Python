034a145fd099af2117e338fd3ced5a199c3ab530
ShipOfLearning <kirankumar.roy@gmail.com>
2025-09-22 19:01:05 +0530

    Update datatype.py
    Corrected Code

--- PythonFullCourse/Class2/datatype.py
+++ PythonFullCourse/Class2/datatype.py
@@ -1,52 +1,52 @@
 # Demonstrating different datatypes in Python
 
-# # Without casting (input returns string, so concatenation happens)
-# a = input("Enter first value: ")
-# b = input("Enter second value: ")
-# print("Result without casting:", a + b)
+# Without casting (input returns string, so concatenation happens)
+a = input("Enter first value: ")
+b = input("Enter second value: ")
+print("Result without casting:", a + b)
 
 # With casting (convert input to integer before addition)
 a_int = int(input("Enter first integer: "))
 b_int = int(input("Enter second integer: "))
 print("Result with casting:", a_int + b_int)
 
-# user_input = input("Enter any value: ")
-# print(f"Value entered: {user_input}")
-# print(f"Type of value: {type(user_input)}")
+user_input = input("Enter any value: ")
+print(f"Value entered: {user_input}")
+print(f"Type of value: {type(user_input)}")
 
-# # Integer
-# age = 25
-# print(f"the type of variable age is {type(age)}")
+# Integer
+age = 25
+print(f"the type of variable age is {type(age)}")
 
-# # Float
-# height = 5.9
-# print(f"the type of variable height is {type(height)}")
+# Float
+height = 5.9
+print(f"the type of variable height is {type(height)}")
 
-# # String
-# name = "Alice"
-# print(f"the type of name height is {type(height)}")
+# String
+name = "Alice"
+print(f"the type of variable name is {type(name)}")
 
-# # Boolean
-# is_student = True
-# print(f"the type of is_student height is {type(height)}")
+# Boolean
+is_student = True
+print(f"the type of variable is_student is {type(is_student)}")
 
 
-# # List
-# fruits = ["apple", "banana", "cherry"]
-# print(f"the type of variable List is {type(height)}")
+# List
+fruits = ["apple", "banana", "cherry"]
+print(f"the type of variable fruits is {type(fruits)}")
 
-# # Tuple
-# coordinates = (10.5, 20.3)
-# print(f"the type of variable coordinates is {type(height)}")
+# Tuple
+coordinates = (10.5, 20.3)
+print(f"the type of variable coordinates is {type(coordinates)}")
 
-# # Dictionary
-# person = {"name": "Bob", "age": 30}
-# print(f"the type of variable Dictionary is {type(height)}")
+# Dictionary
+person = {"name": "Bob", "age": 30}
+print(f"the type of variable person is {type(person)}")
 
-# # Set
-# unique_numbers = {1, 2, 3, 4}
-# print(f"the type of variable unique_numbers is {type(height)}")
+# Set
+unique_numbers = {1, 2, 3, 4}
+print(f"the type of variable unique_numbers is {type(unique_numbers)}")
 
-# # NoneType
-# nothing = None
-# print(f"the type of variable nothing is {type(height)}")
+# NoneType
+nothing = None
+print(f"the type of variable nothing is {type(nothing)}")
