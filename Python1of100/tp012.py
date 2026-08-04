a5a38cbb77544d1d5dd75b6602dd5974b5af33aa
ShipOfLearning <kirankumar.roy@gmail.com>
2026-08-04 09:57:41 +0530

    Add tp012.py string slicing examples
    Add Python1of100/tp012.py: a short tutorial script demonstrating Python string slicing (start:stop:step). The file defines name = "ShipOfLearning" and prints examples: name[:4], name[::-1], name[4::], name[::2], and name[1:10:2].

---
+++ Python1of100/tp012.py
@@ -0,0 +1,10 @@
+## @ShipOfLearning
+## String Slicing (12 of 100)
+## string[start:stop:step]
+name = "ShipOfLearning"
+print(name[:4])
+print(name[::-1])
+print(name[4::])
+print(name[::2])
+
+print(name[1:10:2])
