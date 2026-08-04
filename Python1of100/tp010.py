aeb4ee7daa027449d50476a0920340534363f6d6
ShipOfLearning <kirankumar.roy@gmail.com>
2026-08-04 09:56:25 +0530

    Add Day 10 string basics example
    Add Python1of100/tp010.py demonstrating string basics: defines name, email and a multi-line message, prints length of name, and shows character indexing examples (first and last character) for a sample variable.

---
+++ Python1of100/tp010.py
@@ -0,0 +1,14 @@
+## @ShipOfLearning
+## String Basics (Day 10 of 100)
+
+name = 'KK'
+email = "kk@gmail.com"
+message = '''Welcome to ShipofLearning.
+hope you like this video'''
+
+print(len(name))
+
+var = 'python'
+
+print(var[0])
+print(var[-1])
