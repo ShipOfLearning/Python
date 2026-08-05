aca2c526d571ee9f3258ede839f6e285acc87f60
ShipOfLearning <kirankumar.roy@gmail.com>
2026-08-05 16:37:28 +0530

    Add escape character examples (tp015.py)
    Add Python1of100/tp015.py demonstrating escape characters: newline (\n), tab (\t), escaped double and single quotes, and backslashes in a file path. Part of the 'Python1of100' series (15 of 100).

---
+++ Python1of100/tp015.py
@@ -0,0 +1,10 @@
+## @ShipOfLearning
+## Escape Char in Python (15 of 100)
+
+
+print("My Name is KK and \n I am leaving in India.")
+print("\t My Name is KK and I am leaving in India.")
+print("My Name is KK and \"India is best Country\".")
+print('My Name is KK and \'India is best Country\'.')
+print("c:\\data\\newfolder\\test.txt")
+
