f58d84de6b8c3260a5f0a3ffc4b60485b8fd4c9a
ShipOfLearning <kirankumar.roy@gmail.com>
2026-08-08 10:25:51 +0530

    Clean up formatting in tp015.py
    Remove unnecessary space after tab escape sequence and trailing blank line at end of file.

--- Python1of100/tp015.py
+++ Python1of100/tp015.py
@@ -3,8 +3,7 @@
 
 
 print("My Name is KK and \n I am leaving in India.")
-print("\t My Name is KK and I am leaving in India.")
+print("\tMy Name is KK and I am leaving in India.")
 print("My Name is KK and \"India is best Country\".")
 print('My Name is KK and \'India is best Country\'.')
 print("c:\\data\\newfolder\\test.txt")
-
