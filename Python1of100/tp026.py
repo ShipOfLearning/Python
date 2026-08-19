1965bbfdd940466a1b44c20bcc248b3d8fba821c
ShipOfLearning <kirankumar.roy@gmail.com>
2026-08-19 09:39:12 +0530

    Create tp026.py

---
+++ Python1of100/tp026.py
@@ -0,0 +1,10 @@
+## @ShipOfLearning
+## Nested Lists (Day 26 of 100)
+
+nl = [[1,2,3],["a","b","c"]]
+print(nl[0][0])
+print(nl[1][1])
+
+stud = [["Kiran",[5,98,76]],["Ravi",[75,84,48]]]
+
+print(stud)
