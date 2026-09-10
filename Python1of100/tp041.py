4aa2f4bc8979bbe90012986899b9ceadb46b5b7c
ShipOfLearning <kirankumar.roy@gmail.com>
2026-09-10 10:19:53 +0530

    Create tp041.py

---
+++ Python1of100/tp041.py
@@ -0,0 +1,12 @@
+## @ShipOfLearning (Day 40 of 100)
+##  Nested if
+
+age = 24
+valid_id_proof = False
+if age >= 18:
+    if valid_id_proof:
+        print("Aap vote kar sakte he")
+    else:
+        print("Aap vote nahi kar sakte")    
+else:
+    print("Aap vote nahi kar sakte")
